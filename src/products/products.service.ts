bb2512085eeba951f8375e073961be393fcc77cc
Martinsr99 <silesreche.martin@gmail.com>
2023-11-13 20:19:40 +0100

    feat: create product images

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,6 +6,7 @@ import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import {validate as isUUID} from 'uuid'
+import { ProductImage } from './entities/product-image.entity';
 
 @Injectable()
 export class ProductsService {
@@ -15,22 +16,35 @@ export class ProductsService {
   constructor(
 
     @InjectRepository(Product)
-    private readonly productRepository: Repository<Product>
+    private readonly productRepository: Repository<Product>,
+    
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>
+    
+    ) {}
+    
 
-  ) {}
 
-  async create(createProductDto: CreateProductDto) {
-
-    try {
-      const product = this.productRepository.create(createProductDto)
-      await this.productRepository.save(product)
-
-      return product
-    } catch (error) {
-      this.handleDBExceptions(error)
+    async create(createProductDto: CreateProductDto) {
+    
+      try {
+        const { images = [], ...productDetails } = createProductDto;
+  
+        const product = this.productRepository.create({
+          ...productDetails,
+          images: images.map( image => this.productImageRepository.create({ url: image }) )
+        });
+        
+        await this.productRepository.save( product );
+  
+        return { ...product, images };
+        
+      } catch (error) {
+        this.handleDBExceptions(error);
+      }
+  
+  
     }
-
-  }
 
   async findAll(paginationDto: PaginationDto) {
 
@@ -64,7 +78,7 @@ export class ProductsService {
   async update(id: string, updateProductDto: UpdateProductDto) {
 
     const product = await this.productRepository.preload({
-      id, ...updateProductDto
+      id, ...updateProductDto,images : []
     })
 
     if(!product) throw new NotFoundException(`Product with ${id} not found`)
