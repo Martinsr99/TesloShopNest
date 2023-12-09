904e262c3a4c8eef2caa9a8942ba9ae0f326a4eb
Martinsr99 <silesreche.martin@gmail.com>
2023-12-09 18:37:48 +0100

    feat: add validations to files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helper';
 
 
 @Controller('files')
@@ -8,8 +9,13 @@ export class FilesController {
   constructor(private readonly filesService: FilesService) {}
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(FileInterceptor('file', {fileFilter: fileFilter}))
   uploadProductImage(@UploadedFile() file: Express.Multer.File){
+
+    if(!file) {
+      throw new BadRequestException('There is no file in the request')
+    }
+
     return file
   }
 
