6ff6335a7f674392e7fdb22e9b64c85f1751fede
Martinsr99 <silesreche.martin@gmail.com>
2023-12-11 22:07:58 +0100

    feat: Prepare images to get a link

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
 import { diskStorage } from 'multer';
 import { fileNamer } from './helpers/fileNamer.helper';
+import { Response } from 'express';
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(@Res() res:Response, @Param('imageName') imageName: string) {
+
+    const path = this.filesService.getStaticProductImage(imageName)
+
+    res.sendFile(path)
+  }
 
   @Post('product')
   @UseInterceptors(FileInterceptor('file', {fileFilter: fileFilter,
