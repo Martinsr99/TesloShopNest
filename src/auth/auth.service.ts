1f57f2e32dedbec6dea9e2c9d0f995b4e76138c6
Martinsr99 <silesreche.martin@gmail.com>
2023-12-12 23:54:07 +0100

    feat: install bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import * as bcr from 'bcrypt'
 
 @Injectable()
 export class AuthService {
