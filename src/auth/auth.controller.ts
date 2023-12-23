00bfb5f48dacf97c15a89c3dad7b8085e7fc0fbd
Martinsr99 <silesreche.martin@gmail.com>
2023-12-23 18:37:56 +0100

    feat: create custom guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -23,15 +25,37 @@ export class AuthController {
   @Get('private')
   @UseGuards(AuthGuard())
   testingPrivateRoute(
-    @GetUser() user: User
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
   ){
+    
 
     return {
       ok: true,
       message: 'Hola mundo Private',
-      user
+      user,
+      userEmail,
+      RawHeaders
     }
   }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin','super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  privateRoute2(
+    @GetUser()  user: User
+  ) {
+
+    return {
+      ok: true,
+      user
+    }
+
+  }
+
+
+
 
 
 }
--- src/auth/decorators/raw-headers.decorator.ts
+++ src/auth/decorators/raw-headers.decorator.ts
@@ -1,16 +1,12 @@
 import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
 
 
-export const GetUser = createParamDecorator(
+export const RawHeaders = createParamDecorator(
     (data: string, ctx: ExecutionContext) => {
 
         const req = ctx.switchToHttp().getRequest()
-        const user = req.user
-
-        if(!user)
-            throw new InternalServerErrorException('User not found')
-
-        return (!data) ? user : user[data]
+        
+        return req.RawHeaders
 
     }
 )
