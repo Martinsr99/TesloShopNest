1f2995dff503a892798eaf97530555c8fe376fe6
Martinsr99 <silesreche.martin@gmail.com>
2023-12-24 15:03:51 +0100

    feat: Add tags to documentation as example

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,13 +7,16 @@ import { PaginationDto } from './../common/dtos/pagination.dto';
 import { Auth, GetUser } from '../auth/decorators';
 import { User } from '../auth/entities/user.entity';
 import { ValidRoles } from '../auth/interfaces';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
   @Auth()
+  @ApiResponse({status: 201, description: 'Product was created'})
   create(
     @Body() createProductDto: CreateProductDto,
     @GetUser() user: User,
