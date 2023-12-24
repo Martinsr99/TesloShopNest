100ac50c2013d5e4705f26f19c26f7e0e17ef6e8
Martinsr99 <silesreche.martin@gmail.com>
2023-12-24 17:51:50 +0100

    feat: Add first methods to register and remove clients

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,19 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
 @WebSocketGateway({ cors: true})
-export class MessagesWsGateway {
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket) {
+    this.messagesWsService.registerClient(client)
+
+    console.log({conectados: this.messagesWsService.getConnectedClients()})
+  }
+  
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id)
+    console.log({conectados: this.messagesWsService.getConnectedClients()})
+  }
 }
